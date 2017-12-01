0f80d6923272a89fd97341f690f3d3d585cc1a29
nabilfadjar <nabilfadjar@gmail.com>
2017-12-01 15:32:02 +0000

    Needed to convert string to Int

--- src/ParseRow.scala
+++ src/ParseRow.scala
@@ -19,7 +19,7 @@ class Post(val toBeParsed: String) {
     }
 
     def getId() : Int = {
-        return postMap.get("Id").getOrElse(-1)
+        return postMap.get("Id").getOrElse("-1").toInt
     }
 
     def getBody() : String = {
