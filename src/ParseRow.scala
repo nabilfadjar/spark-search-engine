ef8ce64273668e2d775c731f0bc0f28f2f72b407
nabilfadjar <nabilfadjar@gmail.com>
2017-12-01 14:45:57 +0000

    Filter out the header and footer

--- src/ParseRow.scala
+++ src/ParseRow.scala
@@ -1,23 +1,27 @@
 import scala.io.Source
 
 class Post(val toBeParsed: String) {
-   private var postMap = transformIntoMap()
+    private var postMap = if(isHeaderOrFooter()) null else transformIntoMap()
 
-   private def transformIntoMap() : Map[String, String] = {
-     var preParsedRow = toBeParsed.split('"').map(_.replace("<row","")).map(_.replace("/>","")).map(_.trim).filterNot(_.isEmpty)
-     var postHeader = preParsedRow.filter(_.endsWith("=")).map(_.replace("=", ""))
-     var postContent = preParsedRow.filterNot(_.endsWith("="))
-     return (postHeader zip postContent).toMap
-   }
+    private def transformIntoMap() : Map[String, String] = {
+        var preParsedRow = toBeParsed.split('"').map(_.replace("<row","")).map(_.replace("/>","")).map(_.trim).filterNot(_.isEmpty)
+        var postHeader = preParsedRow.filter(_.endsWith("=")).map(_.replace("=", ""))
+        var postContent = preParsedRow.filterNot(_.endsWith("="))
+        return (postHeader zip postContent).toMap
+    }
 
-   def getPost() :Map[String,String] = {
-      return postMap
-   }
+    private def isHeaderOrFooter() : Boolean = {
+        return (toBeParsed.contains("<?xml version=\"1.0\" encoding=\"utf-8\"?>") || toBeParsed.endsWith("posts>"))
+    }
+
+    def getPost() :Map[String,String] = {
+        return postMap
+    }
 }
 
 def readXMLFile() : Array[String] = {
-   val xmlSource = Source.fromFile("sample_data/Posts.xml")
-   val posts = xmlSource.getLines.toArray
-   xmlSource.close
-   return posts
+    val xmlSource = Source.fromFile("sample_data/Posts.xml")
+    val posts = xmlSource.getLines.toArray
+    xmlSource.close
+    return posts
 }
