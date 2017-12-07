e3f83cee8f1969311820f112180749865bcff0e0
Muhammad Nabil Fadhiya <nabilfadjar@gmail.com>
2017-12-07 23:10:23 +0000

    Renamed getPost to getMap; now we can getWordsFromBody

--- scala-local-test/ParseRow.scala
+++ scala-local-test/ParseRow.scala
@@ -6,17 +6,18 @@ class Post(val toBeParsed: String) {
     private var postMap = if(isHeaderOrFooter()) null else transformIntoMap()
 
     private def transformIntoMap() : Map[String, String] = {
-        var preParsedRow = toBeParsed.split('"').map(_.replace("<row","")).map(_.replace("/>","")).map(_.trim).filterNot(_.isEmpty)
-        var postHeader = preParsedRow.filter(_.endsWith("=")).map(_.replace("=", ""))
-        var postContent = preParsedRow.filterNot(_.endsWith("="))
-        return (postHeader zip postContent).toMap
+        // var preParsedRow = toBeParsed.split('"').map(_.replace("<row","")).map(_.replace("/>","")).map(_.trim).filterNot(_.isEmpty)
+        // var postHeader = preParsedRow.filter(_.endsWith("=")).map(_.replace("=", ""))
+        // var postContent = preParsedRow.filterNot(_.endsWith("="))
+        // return (postHeader zip postContent).toMap
+        return toBeParsed.split("(=\")|(\"[\\s])|(<[\\w]*)|(/>)").map(_.trim).filter(_.nonEmpty).grouped(2).collect { case Array(k, v) => k -> v }.toMap
     }
 
     private def isHeaderOrFooter() : Boolean = {
         return (toBeParsed.contains("<?xml version=\"1.0\" encoding=\"utf-8\"?>") || toBeParsed.endsWith("posts>"))
     }
 
-    def getPost() :Map[String,String] = {
+    def getMap() : Map[String,String] = {
         return postMap
     }
 
@@ -26,6 +27,10 @@ class Post(val toBeParsed: String) {
 
     def getBody() : String = {
         return postMap.get("Body").getOrElse(null)
+    }
+
+    def getWordsFromBody() : Array[String] = {
+        if (getBody() == null) return null else return getBody().toLowerCase.replaceAll("&lt;code&gt;", "").replaceAll("(&[\\S]*;)|(&lt;[\\S]*&gt;)", " ").replaceAll("[\\s](a href)|(rel)[\\s]", " ").replaceAll("(?!([\\w]*'[\\w]))([\\W_\\s\\d])+"," ").split(" ").filter(_.nonEmpty)
     }
 }
 
--- src/ParseRow.scala
+++ src/ParseRow.scala
@@ -2,17 +2,18 @@ class Post(val toBeParsed: String) {
     private var postMap = if(isHeaderOrFooter()) null else transformIntoMap()
 
     private def transformIntoMap() : Map[String, String] = {
-        var preParsedRow = toBeParsed.split('"').map(_.replace("<row","")).map(_.replace("/>","")).map(_.trim).filterNot(_.isEmpty)
-        var postHeader = preParsedRow.filter(_.endsWith("=")).map(_.replace("=", ""))
-        var postContent = preParsedRow.filterNot(_.endsWith("="))
-        return (postHeader zip postContent).toMap
+        // var preParsedRow = toBeParsed.split('"').map(_.replace("<row","")).map(_.replace("/>","")).map(_.trim).filterNot(_.isEmpty)
+        // var postHeader = preParsedRow.filter(_.endsWith("=")).map(_.replace("=", ""))
+        // var postContent = preParsedRow.filterNot(_.endsWith("="))
+        // return (postHeader zip postContent).toMap
+        return toBeParsed.split("(=\")|(\"[\\s])|(<[\\w]*)|(/>)").map(_.trim).filter(_.nonEmpty).grouped(2).collect { case Array(k, v) => k -> v }.toMap
     }
 
     private def isHeaderOrFooter() : Boolean = {
         return (toBeParsed.contains("<?xml version=\"1.0\" encoding=\"utf-8\"?>") || toBeParsed.endsWith("posts>"))
     }
 
-    def getPost() :Map[String,String] = {
+    def getMap() : Map[String,String] = {
         return postMap
     }
 
@@ -23,10 +24,8 @@ class Post(val toBeParsed: String) {
     def getBody() : String = {
         return postMap.get("Body").getOrElse(null)
     }
-}
 
-// Quick Code
-// var posts = readXMLFile.map(row => new Post(row)).filterNot(_.getPost() == null)
-// var wordTuple = posts.flatMap(_.getBody.split(" ")).map(word => (word,1)).reduceByKey(word,count => (word+count))
-// var idf = (Math.log(totalPosts) - Math.log(wordCount))/Math.log(Math.E)
-// var idfArr = reduced.map(eachTuple => (Math.log(totalPosts) - Math.log(eachTuple._2))/Math.log(Math.E))
+    def getWordsFromBody() : Array[String] = {
+        if (getBody() == null) return null else return getBody().toLowerCase.replaceAll("&lt;code&gt;", "").replaceAll("(&[\\S]*;)|(&lt;[\\S]*&gt;)", " ").replaceAll("[\\s](a href)|(rel)[\\s]", " ").replaceAll("(?!([\\w]*'[\\w]))([\\W_\\s\\d])+"," ").split(" ").filter(_.nonEmpty)
+    }
+}
