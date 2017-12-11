d96163742b61fa1d27d51218b6e7ff4e79fd5a07
Muhammad Nabil Fadhiya <nabilfadjar@gmail.com>
2017-12-11 00:08:15 +0000

    Revert "Revert "File to create tf-idf""
    This reverts commit 89e3df77e9f54d7ef5393f65a5086e2866c45f95.

--- src/ParseRow.scala
+++ src/ParseRow.scala
@@ -1,5 +1,6 @@
 class Post(val toBeParsed: String) {
-    private var postMap = if(isHeaderOrFooter()) null else transformIntoMap()
+    private val postMap = if(isHeaderOrFooter()) null else transformIntoMap()
+    private val wordsInBody = extractWordsFromBody()
 
     private def transformIntoMap() : Map[String, String] = {
         return toBeParsed.split("(=\")|(\"[\\s])|(<[\\w]*)|(/>)").map(_.trim).filter(_.nonEmpty).grouped(2).collect { case Array(k, v) => k -> v }.toMap
@@ -14,14 +15,22 @@ class Post(val toBeParsed: String) {
     }
 
     def getId() : Int = {
-        return postMap.get("Id").getOrElse("-1").toInt
+        if (getMap() == null) return -1 else return postMap.get("Id").getOrElse("-1").toInt
     }
 
     def getBody() : String = {
-        return postMap.get("Body").getOrElse(null)
+        if (getMap() == null) return null else return postMap.get("Body").getOrElse(null)
+    }
+
+    private def extractWordsFromBody() : Array[String] = {
+        if (getBody() == null) return null else return getBody().toLowerCase.replaceAll("&lt;code&gt;", "").replaceAll("(&[\\S]*;)|(&lt;[\\S]*&gt;)", " ").replaceAll("[\\s](a href)|(rel)[\\s]", " ").replaceAll("(?!([\\w]*'[\\w]))([\\W_\\s\\d])+"," ").split(" ").filter(_.nonEmpty)
     }
 
     def getWordsFromBody() : Array[String] = {
-        if (getBody() == null) return null else return getBody().toLowerCase.replaceAll("&lt;code&gt;", "").replaceAll("(&[\\S]*;)|(&lt;[\\S]*&gt;)", " ").replaceAll("[\\s](a href)|(rel)[\\s]", " ").replaceAll("(?!([\\w]*'[\\w]))([\\W_\\s\\d])+"," ").split(" ").filter(_.nonEmpty)
+        if (wordsInBody == null) return null else return wordsInBody
+    }
+
+    def getNumberOfWordsInPost() : Int = {
+        return getWordsFromBody().length
     }
 }
