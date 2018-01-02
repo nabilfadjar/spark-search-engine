159e31b09f64b7083e83b9f5febd17038c63cf52
nabilfadjar <nabilfadjar@gmail.com>
2018-01-02 22:00:33 +0000

    Using lcd

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,7 @@
 #
 
 from fabric.api import local, env, run
+from fabric.context_managers import lcd
 
 def package():
     print("Packaging all Scala Projects into JAR files")
@@ -11,32 +12,32 @@ def package():
 
 def package_tf_idf():
     print("Packaging TF-IDF into JAR files...")
-    with cd('GenerateTfIdf/'):
+    with lcd('GenerateTfIdf/'):
         local("sbt clean package")
 
 def package_query_engine():
     print("Packaging all Query Engine into JAR file...")
-    with cd('QueryEngine/'):
+    with lcd('QueryEngine/'):
         local("sbt clean package")
 
 def run_tf_idf():
     print("[MAIN] Sumbitting job to Spark for Generating TF-IDF...")
-    with cd('GenerateTfIdf/target/scala-2.10'):
+    with lcd('GenerateTfIdf/target/scala-2.10'):
         local("spark-submit generatetfidf_2.10-1.0.0-SNAPSHOT.jar --main >> logs/spark.log &")
 
 def run_sample_tf_idf():
     print("[SAMPLE] Sumbitting job to Spark for Generating TF-IDF...")
-    with cd('GenerateTfIdf/target/scala-2.10'):
+    with lcd('GenerateTfIdf/target/scala-2.10'):
         local("spark-submit generatetfidf_2.10-1.0.0-SNAPSHOT.jar --sample >> logs/spark.log &")
 
 def run_query_engine():
     print("[MAIN] Sumbitting job to Spark for Query Engine...")
     query = prompt("Query?")
-    with cd('QueryEngine/target/scala-2.10'):
+    with lcd('QueryEngine/target/scala-2.10'):
         local("spark-submit queryengine_2.10-1.0.0-SNAPSHOT.jar --main \"%s\" >> logs/spark.log &" % (query) )
 
 def run_sample_query_engine():
     print("[SAMPLE] Packaging all Query Engine into Query Engine...")
     query = prompt("Query?")
-    with cd('QueryEngine/target/scala-2.10'):
+    with lcd('QueryEngine/target/scala-2.10'):
         local("spark-submit queryengine_2.10-1.0.0-SNAPSHOT.jar --sample \"%s\" >> logs/spark.log &" % (query))
